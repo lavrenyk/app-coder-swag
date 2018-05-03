45b8ed5ca4297312ae9cd5e514d06b727312fb5f
MoHapX <lavrenyk@gmail.com>
2018-05-03 21:42:05 +0300

    finish the app

--- coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/Controller/CategoriesVC.swift
@@ -8,20 +8,53 @@
 
 import UIKit
 
-class CategoriesVC: UIViewController {
+class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var categoryTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        categoryTable.dataSource = self
+        categoryTable.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.getCategories().count 
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {
+            let category = DataService.instance.getCategories()[indexPath.row]
+            cell.updateView(category: category)
+            return cell
+        } else {
+            return CategoryCell()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductsVC", sender: category)
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productVC = segue.destination as? ProductsVC {
+            let barBtn = UIBarButtonItem()
+            barBtn.title = ""
+            navigationItem.backBarButtonItem = barBtn
+            
+            assert(sender as? Category != nil)
+            productVC.initProduct(category: sender as! Category)
+         }
+    }
 
 }
+
+
+
+
+
+
+
+
 
---
+++ coder-swag/Services/DataService.swift
@@ -0,0 +1,91 @@
+//
+//  DataService.swift
+//  coder-swag
+//
+//  Created by MoHapX on 03.05.2018.
+//  Copyright © 2018 MoHapX. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    private let hats = [
+        Product(title: "Devslopes Logo Graphic Beanie", price: "$18", imageName: "hat01.png"),
+        Product(title: "Devslopes Logo Hat Black", price: "$22", imageName: "hat02.png"),
+        Product(title: "Devslopes Logo Hat White", price: "$22", imageName: "hat03.png"),
+        Product(title: "Devslopes Logo Snapback", price: "$20", imageName: "hat04.png")
+    ]
+    
+    private let hoodies = [
+        Product(title: "Devslopes Logo Hoodie Gray", price: "$32", imageName: "hoodie01.png"),
+        Product(title: "Devslopes Logo Hoodie Red", price: "$32", imageName: "hoodie02.png"),
+        Product(title: "Devslopes Hoodie Gray", price: "$32", imageName: "hoodie03.png"),
+        Product(title: "Devslopes Hoodie Black", price: "$32", imageName: "hoodie04.png")
+    ]
+    
+    private let shirts = [
+        Product(title: "Devslopes Logo Shirt Black", price: "$18", imageName: "shirt01.png"),
+        Product(title: "Devslopes Badge Shirt Light Gray", price: "$19", imageName: "shirt02.png"),
+        Product(title: "Devslopes Logo Shirt Red", price: "$20", imageName: "shirt03.png"),
+        Product(title: "Hustle Deligate Gray", price: "$21", imageName: "shirt04.png"),
+        Product(title: "Kickflip Studios Black", price: "$22", imageName: "shirt05.png")
+    ]
+    
+    private let digitalGoods = [Product]()
+    
+    func getCategories() -> [Category] {
+        return categories
+    }
+    
+    func getProducts(forCategoryTitle title: String) -> [Product] {
+        switch title {
+        case "SHIRTS":
+            return getShirts()
+        case "HOODIES":
+            return getHoodies()
+        case "HATS":
+            return getHats()
+        case "DIGITAL":
+            return getDigitalProducts()
+        default:
+            return getShirts()
+        }
+    }
+    
+    func getHats() -> [Product] {
+        return hats
+    }
+    
+    func getHoodies() -> [Product] {
+        return hoodies
+    }
+    
+    func getShirts() -> [Product] {
+        return shirts
+    }
+    
+    func getDigitalProducts() -> [Product] {
+        return digitalGoods
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
